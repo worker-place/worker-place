621383f33bca1760b4931f5d1af52dc9eebf36db
DaniFoldi <dani@stefan.hu>
2022-05-19 21:58:41 +0100

    :zap: Change paintbrush properties

--- packages/page/app.vue
+++ packages/page/app.vue
@@ -1,15 +1,17 @@
 <template>
   <Paintbrush
-    app-name="Paintbrush UI Docs"
-    canonical="https://paintbrush.danifoldi.com"
-    description="View the documentation for the Paintbrush UI component library"
-    favicon="/favicon.png"
-    favicon-svg="/paintbrush.svg"
-    large-image="/paintbrush.png"
+    app-name="The internet worker.place"
+    canonical="https://worker.place"
+    :color-scheme="colorScheme"
+    :dark-colors="darkColors"
+    description="Contribute to the worker.place"
+    favicon="/icon.png"
+    favicon-svg="/icon.svg"
+    large-image="/icon.png"
     large-image-alt="Paintbrush UI Logo"
-    page-title="Paintbrush UI Docs"
-    twitter-handle="danifoldi"
-    url="https://paintbrush.danifoldi.com"
+    :light-colors="lightColors"
+    page-title="The internet worker.place"
+    url="https://worker.place"
   >
     <NuxtPage />
   </Paintbrush>
@@ -20,4 +22,38 @@
 
 
   useNuxtApp().vueApp.config.unwrapInjectedRef = true
+
+  const colorScheme = {
+    black: '#021d27',
+    blue: '#549ab6',
+    dark: '#022b3a',
+    green: '#1f7a8c',
+    lightblue: '#bfdbf7',
+    lightgray: '#e1e5f2',
+    white: '#ffffff'
+  }
+  const lightColors = {
+    background: 'lightgray',
+    background2: 'white',
+    doubleUnit: '8px',
+    primary: 'green',
+    radius: '4px',
+    secondary: 'black',
+    text: 'black',
+    textOnPrimary: 'white',
+    textShade: 'lightgray',
+    unit: '4px'
+  }
+  const darkColors = {
+    background: 'black',
+    background2: 'dark',
+    doubleUnit: '8px',
+    primary: 'green',
+    radius: '4px',
+    secondary: 'blue',
+    text: 'white',
+    textOnPrimary: 'white',
+    textShade: 'blue',
+    unit: '4px'
+  }
 </script>
