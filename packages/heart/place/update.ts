41c62a0dc09aad2fe79ba0fe705106a50365b7a6
DaniFoldi <dani@stefan.hu>
2022-05-21 14:17:17 +0100

    :zap: Implement draw function

--- packages/heart/place/update.ts
+++ packages/heart/place/update.ts
@@ -1,8 +1,16 @@
 import { PlaceEnvironment, SquadMeta, SquadTarget } from './types'
 
 
+function targetToImageIndex(index: number, target: SquadTarget): number {
+  return (target.top + Math.floor(index / target.height)) * 1024 + target.left + index % target.width
+}
+
 function draw(target: SquadTarget, meta: SquadMeta, image: Uint8Array): number {
-  // todo update pixels
+  let pixel = meta.nextPixel
+  for (let i = 0; i < meta.memberCount; i++) {
+    image[targetToImageIndex(pixel, target)] = target.target[pixel]
+    pixel = pixel % (1024 * 1024)
+  }
   return (meta.nextPixel + meta.memberCount) % (target.width * target.height)
 }
 
