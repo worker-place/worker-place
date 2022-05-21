5425087dfd46be950d675bc22f46e246169ae9fa
András Ádám <adm.andras@gmail.com>
2022-05-21 23:26:58 +0300

    :zap: Parse png images

--- packages/heart/src/index.ts
+++ packages/heart/src/index.ts
@@ -1,10 +1,22 @@
 import { comet, Method, useComet } from '@neoaren/comet'
+import { PNG } from 'pngjs/browser'
 
 
-useComet({
+useComet<unknown, { image: File }>({
   method: Method.POST,
   pathname: '/api'
 }, async event => {
+  const png = new PNG()
+  const raw = await event.body.image.arrayBuffer()
+  const data = await new Promise<Uint8Array>((resolve, reject) => {
+    png.parse(raw, (error, data) => {
+      if (error) return reject(error)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      return resolve(data.data as Uint8Array)
+    })
+  })
+  console.log('data', data)
   return event.reply.ok({ message: 'Hello, Comet!' })
 })
 
