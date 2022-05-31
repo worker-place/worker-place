86f6fc2c74aac2664006d1e7915b80f0ed3f45d4
DaniFoldi <dani@stefan.hu>
2022-05-31 21:59:36 +0100

    :zap: Only connect to ws in /

--- packages/page/pages/index.vue
+++ packages/page/pages/index.vue
@@ -42,17 +42,3 @@
     </template>
   </SidebarContainer>
 </template>
-
-<script lang="ts" setup>
-  onMounted(() => {
-    const socket = new WebSocket('wss://worker.place/api/connect')
-
-    socket.addEventListener('open', () => {
-      console.log('connected')
-    })
-
-    socket.addEventListener('message', event => {
-      console.log(event)
-    })
-  })
-</script>
---
+++ packages/page/pages/index/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+  onMounted(() => {
+    const socket = new WebSocket('wss://worker.place/api/connect')
+
+    socket.addEventListener('open', () => {
+      console.log('connected')
+    })
+
+    socket.addEventListener('message', event => {
+      console.log(event)
+    })
+  })
+</script>
