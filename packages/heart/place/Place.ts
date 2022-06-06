1183bf7af40518ce19d6e5bad04be2b9100de0b9
DaniFoldi <dani@stefan.hu>
2022-06-06 23:23:00 +0100

    :hammer: I need a chainsaw not a hammer

--- packages/heart/place/Place.ts
+++ packages/heart/place/Place.ts
@@ -25,21 +25,21 @@ export class Place {
   }
 
   async loadImage() {
-    for (let i = 0; i < 49; i++) {
+    for (let i = 0; i < 384; i++) {
       const piece = await this.state.storage.get<Uint8Array>(`stored_image_${i}`)
       if (!piece) {
         // deliberate overhead, max would be 128*1024
-        this.mem.IMAGE?.fill(4, i * 65000, (i + 1) * 65000)
+        this.mem.IMAGE?.fill(4, i * 8192, (i + 1) * 8192)
       } else {
-        this.mem.IMAGE?.set(piece, i * 65000)
+        this.mem.IMAGE?.set(piece, i * 8192)
       }
     }
   }
 
   async saveImage() {
     const writes = []
-    for (let i = 0; i < 49; i++) {
-      const piece = i === 48 ? this.mem.IMAGE?.subarray(i * 65000) : this.mem.IMAGE?.subarray(i * 65000, (i + 1) * 65000)
+    for (let i = 0; i < 384; i++) {
+      const piece = i === 383 ? this.mem.IMAGE?.subarray(i * 8192) : this.mem.IMAGE?.subarray(i * 65000, (i + 1) * 8192)
       writes.push(this.state.storage.put(`stored_image_${i}`, piece))
     }
     await Promise.all(writes)
