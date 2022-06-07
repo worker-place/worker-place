e878e43c01271abdbbbdf89a9c907dd8b0890d4a
Tamás Ádám <atomi2500@gmail.com>
2022-06-07 19:02:20 +0300

    :hammer: set content-type

--- packages/backup/src/index.ts
+++ packages/backup/src/index.ts
@@ -7,6 +7,9 @@ async function doBackup(env: BackupEnvironment): Promise<unknown> {
       const value = await env.SNAPSHOTS.get(key)
       pending.push(env.HEART.fetch('https://worker.place/api/backup', {
         method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
         body: JSON.stringify({ key: key, value: value })
       }))
     }
