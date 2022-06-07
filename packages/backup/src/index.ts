5e39b7ceed9a14eb7095fd745664caf777c2807e
Tamás Ádám <atomi2500@gmail.com>
2022-06-07 12:03:40 +0300

    :hammer: logging

--- packages/backup/src/index.ts
+++ packages/backup/src/index.ts
@@ -2,14 +2,26 @@ import { PNG } from 'pngjs/browser'
 
 
 async function doBackup(env: BackupEnvironment): Promise<unknown> {
-  console.log('hello')
+  console.log('backup...')
   // eslint-disable-next-line unicorn/no-await-expression-member
   const keys = (await env.SNAPSHOTS.list()).keys.map(key => key.name)
+  console.log('keys: ')
+  console.log(keys)
   const values = await Promise.all(keys.map(each => env.SNAPSHOTS.get(each, { type: 'arrayBuffer' })))
+  console.log('Values:')
+  console.log(values)
   await Promise.all(values.filter(Boolean)
-    // @ts-expect-error ...
-    .map(each => new PNG().end(each).pack().data)
-    .map(each => env.BACKUP.put(new Date().toISOString(), each)))
+    .map(each => {
+      // @ts-expect-error ...
+      const result = new PNG().end(each).pack().data
+      console.log('data is:')
+      console.log(result)
+    })
+    .map(each => {
+      // @ts-expect-error ...
+      env.BACKUP.put(new Date().toISOString(), each)
+      console.log('updated')
+    }))
 
   return await Promise.all(keys.map(each => env.SNAPSHOTS.delete(each)))
 }
