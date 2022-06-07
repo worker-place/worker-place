d255f8959a2ddf2bb8168edff61d314881bdc5b0
Tamás Ádám <atomi2500@gmail.com>
2022-06-07 16:06:04 +0300

    :hammer: remove debug stuff

--- packages/backup/src/index.ts
+++ packages/backup/src/index.ts
@@ -5,20 +5,9 @@ async function doBackup(env: BackupEnvironment): Promise<unknown> {
   console.log('backup...')
   // eslint-disable-next-line unicorn/no-await-expression-member
   const keys = (await env.SNAPSHOTS.list()).keys.map(key => key.name).splice(0, 10)
-  console.log('keys: ')
-  console.log(keys)
   if (keys.length > 0) {
-    console.log('keys.length > 0')
     const values = await Promise.all(keys.map(each => env.SNAPSHOTS.get(each)))
-    console.log('Values:')
-    console.log(JSON.stringify(values))
-    console.log('-----')
-    // @ts-expect-error ...
-    values.forEach(each => console.log(Object.keys(each)))
-    console.log('abc')
-    console.log('def')
-    console.log('not doing anything...')
-    /*await Promise.all(values.filter(Boolean)
+    await Promise.all(values.filter(Boolean)
       .map(each => {
         // @ts-expect-error ...
         const result = new PNG().end(each).pack().data
@@ -29,7 +18,7 @@ async function doBackup(env: BackupEnvironment): Promise<unknown> {
         // @ts-expect-error ...
         env.BACKUP.put(new Date().toISOString(), each)
         console.log('updated')
-      }))*/
+      }))
 
     return await Promise.all(keys.map(each => env.SNAPSHOTS.delete(each)))
   }
