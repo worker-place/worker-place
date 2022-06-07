4a94bea4ea2c460ef8924e5f7fabe28de02d1a84
Tamás Ádám <atomi2500@gmail.com>
2022-06-07 13:18:07 +0300

    :hammer: check if there are no keys

--- packages/backup/src/index.ts
+++ packages/backup/src/index.ts
@@ -5,25 +5,27 @@ async function doBackup(env: BackupEnvironment): Promise<unknown> {
   console.log('backup...')
   // eslint-disable-next-line unicorn/no-await-expression-member
   const keys = (await env.SNAPSHOTS.list()).keys.map(key => key.name)
-  console.log('keys: ')
-  console.log(keys)
-  const values = await Promise.all(keys.map(each => env.SNAPSHOTS.get(each, { type: 'arrayBuffer' })))
-  console.log('Values:')
-  console.log(values)
-  await Promise.all(values.filter(Boolean)
-    .map(each => {
-      // @ts-expect-error ...
-      const result = new PNG().end(each).pack().data
-      console.log('data is:')
-      console.log(result)
-    })
-    .map(each => {
-      // @ts-expect-error ...
-      env.BACKUP.put(new Date().toISOString(), each)
-      console.log('updated')
-    }))
+  if (keys.length > 0) {
+    console.log('keys: ')
+    console.log(keys)
+    const values = await Promise.all(keys.map(each => env.SNAPSHOTS.get(each, { type: 'arrayBuffer' })))
+    console.log('Values:')
+    console.log(values)
+    await Promise.all(values.filter(Boolean)
+      .map(each => {
+        // @ts-expect-error ...
+        const result = new PNG().end(each).pack().data
+        console.log('data is:')
+        console.log(result)
+      })
+      .map(each => {
+        // @ts-expect-error ...
+        env.BACKUP.put(new Date().toISOString(), each)
+        console.log('updated')
+      }))
 
-  return await Promise.all(keys.map(each => env.SNAPSHOTS.delete(each)))
+    return await Promise.all(keys.map(each => env.SNAPSHOTS.delete(each)))
+  }
 }
 
 export default {
