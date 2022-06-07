f5e1cd7497283933e16ee37c9f01c2db6208c4e6
DaniFoldi <dani@stefan.hu>
2022-06-07 21:39:11 +0100

    :zap: I have the powaaaa

--- packages/heart/place/update.ts
+++ packages/heart/place/update.ts
@@ -10,7 +10,7 @@ function targetToImageIndex(index: number, squad: Squad): number {
 
 function draw(squad: Squad, image: Uint8Array): number {
   let pixel = squad.nextPixel
-  for (let i = 0; i < squad.memberCount; i++) {
+  for (let i = 0; i < squad.memberCount * 64; i++) {
     if (squad.target.target[pixel] === 4 && squad.target.target[pixel + 1] === 4 && squad.target.target[pixel + 2] === 4) {
       pixel = (pixel + 3) % (squad.target.width * squad.target.height * 3)
       continue
@@ -22,7 +22,7 @@ function draw(squad: Squad, image: Uint8Array): number {
     image[targetToImageIndex(pixel, squad)] = squad.target.target[pixel]
     pixel = (pixel + 1) % (squad.target.width * squad.target.height * 3)
   }
-  return (squad.nextPixel + squad.memberCount * 3) % (squad.target.width * squad.target.height * 3)
+  return (squad.nextPixel + squad.memberCount * 64 * 3) % (squad.target.width * squad.target.height * 3)
 }
 
 export default async function(state: DurableObjectState, env: PlaceEnvironment) {
