a26384f0cfd8fa7fd931f588bd54585bc7b83e2e
DaniFoldi <dani@stefan.hu>
2022-06-07 15:48:57 +0100

    :zap: Add exponential backoff ws reconnects

--- packages/page/pages/index/index.vue
+++ packages/page/pages/index/index.vue
@@ -36,33 +36,52 @@
       observer.observe(canvas.value?.parentElement)
     }
 
-    const socket = new WebSocket('wss://worker.place/api/connect')
+    let socket: WebSocket
+    let delay = 0
 
-    socket.addEventListener('open', () => {
-      console.log('connected')
-
-      socket.send('test')
-    })
-
-    socket.addEventListener('message', async event => {
-      if (event.data) {
-        const buffer = await (event.data as Blob).arrayBuffer()
-        const uintBuffer = new Uint8Array(buffer)
-        lastbuffer = new Uint8ClampedArray(4 * 1024 * 1024)
-        lastbuffer.fill(255)
-        console.log(uintBuffer)
-        for (let i = 0; i < 1024 * 1024; i++) {
-          if (uintBuffer[3 * i] === 4 && uintBuffer[3 * i + 1] === 4 && uintBuffer[3 * i + 2] === 4) {
-            lastbuffer[4 * i + 3] = 0
-          } else {
-            lastbuffer[4 * i] = uintBuffer[3 * i]
-            lastbuffer[4 * i + 1] = uintBuffer[3 * i + 1]
-            lastbuffer[4 * i + 2] = uintBuffer[3 * i + 2]
-          }
-        }
-        context.putImageData(new ImageData(lastbuffer, 1024, 1024), 0, 0, 0, 0, size, size)
+    async function connectAfterDelay() {
+      if (delay > 20000) {
+        return
       }
-    })
+      await new Promise(resolve => setTimeout(resolve, delay))
+
+      socket = new WebSocket('ws://localhost:8787/api/connect')
+
+      socket.addEventListener('open', () => {
+        console.log('connected')
+
+        socket.send('hello')
+        delay = 500
+      })
+
+      socket.addEventListener('message', async event => {
+        if (event.data) {
+          const buffer = await (event.data as Blob).arrayBuffer()
+          const uintBuffer = new Uint8Array(buffer)
+          lastbuffer = new Uint8ClampedArray(4 * 1024 * 1024)
+          lastbuffer.fill(255)
+          for (let i = 0; i < 1024 * 1024; i++) {
+            if (uintBuffer[3 * i] === 4 && uintBuffer[3 * i + 1] === 4 && uintBuffer[3 * i + 2] === 4) {
+              lastbuffer[4 * i + 3] = 0
+            } else {
+              lastbuffer[4 * i] = uintBuffer[3 * i]
+              lastbuffer[4 * i + 1] = uintBuffer[3 * i + 1]
+              lastbuffer[4 * i + 2] = uintBuffer[3 * i + 2]
+            }
+          }
+          context.putImageData(new ImageData(lastbuffer, 1024, 1024), 0, 0, 0, 0, size, size)
+        }
+      })
+
+      socket.addEventListener('close', async event => {
+        if (!event.wasClean) {
+          delay *= 2
+        }
+        connectAfterDelay()
+      })
+    }
+
+    connectAfterDelay()
   })
 
   onBeforeUnmount(() => {
